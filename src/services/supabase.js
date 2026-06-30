df6972c08d108b0f57c5363382b5e3b58e900730
Amanda Prado Seibel <amanda.seibel@aelbra.com.br>
2026-06-29 21:11:30 -0300

    Corrige carregamento do Supabase no Vercel

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL ||
+  "https://ahattpqrjmhkzsmnbdzs.supabase.co";
+
+const supabaseAnonKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY ||
+  "sb_publishable_DOYQQCgscKWJGYXZ64BKkQ_yvk_ZNW3";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
